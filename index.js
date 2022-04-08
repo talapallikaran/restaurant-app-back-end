dd86d83dbd32ef6a08a32b223420a3ab220749d1
Rumit Gajjar <rumit.g@aimsinfosoft.com>
2022-04-08 10:52:57 +0530

    backend initial push

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const db = require("./queries");
+const PORT = process.env.PORT || 8080;
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+app.get("/", (request, response) => {
+  response.json({ info: "Node.js, Express, and Postgres API" });
+});
+
+app.get("/users", db.getUsers);
+app.get("/users/:id", db.getUserById);
+app.post("/users", db.createUser);
+app.put("/users/:id", db.updateUser);
+app.delete("/userDelete", db.deleteUser);
+app.post("/otp", db.checkOtp);
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}.`);
+});
---
+++ queries.js
@@ -0,0 +1,112 @@
+const Pool = require("pg").Pool;
+const pool = new Pool({
+  user: "me",
+  host: "localhost",
+  database: "api",
+  password: "password",
+  port: 5432,
+});
+
+const getUsers = (request, response) => {
+  pool.query("SELECT * FROM users ORDER BY id ASC", (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).json(results.rows);
+  });
+};
+
+const getUserById = (request, response) => {
+  const id = parseInt(request.params.id);
+
+  pool.query("SELECT * FROM users WHERE id = $1", [id], (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).json(results.rows);
+  });
+};
+
+const createUser = (request, response) => {
+  const { phone } = request.body;
+  const otp = Math.floor(1000 + Math.random() * 9000);
+
+  pool.query(
+    "INSERT INTO users (phone,otp) VALUES ($1,$2)",
+    [phone, otp],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      response
+        .status(200)
+        .json({ status: "success", message: `User successfully added` });
+    }
+  );
+};
+
+const updateUser = (request, response) => {
+  const id = parseInt(request.params.id);
+  const { phone } = request.body;
+
+  pool.query(
+    "UPDATE users SET name = $1, email = $2 WHERE id = $3",
+    [phone, id],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      response
+        .status(200)
+        .json({ status: "success", message: `User successfully modified` });
+    }
+  );
+};
+
+const deleteUser = (request, response) => {
+  const { phone } = parseInt(request.body);
+
+  pool.query("DELETE FROM users WHERE id = $1", [phone], (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).json({
+      status: "success",
+      message: "User successfully deleted!",
+    });
+  });
+};
+
+const checkOtp = (request, response) => {
+  const { phone, otp } = request.body;
+
+  pool.query(
+    "SELECT (phone,otp) FROM users WHERE phone IN ($1) AND otp IN ($2) ORDER BY id DESC LIMIT 1",
+    [phone, otp],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      if (results.rows.length > 0) {
+        response
+          .status(200)
+          .json({ status: "success", message: "Otp verified" });
+        // pool.query("UPDATE users SET otp = '' WHERE phone = $1");
+      } else {
+        response.status(400).json({
+          status: "failed",
+          message: "Check phone number and otp again",
+        });
+      }
+    }
+  );
+};
+
+module.exports = {
+  getUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+  checkOtp,
+};
