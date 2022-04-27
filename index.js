9ac3c81cb22f333f1c87e12fd63a9d9aee74371b
Darshan Vasa <darshan.v@aimsinfosoft.com>
2022-04-27 17:25:43 +0530

    restaurant-api-implmention

--- index.js
+++ index.js
@@ -4,6 +4,27 @@ const app = express();
 const db = require("./queries");
 const PORT = process.env.PORT || 8080;
 
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+
+// get config vars
+dotenv.config();
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  if (token == null) return res.sendStatus(401);
+
+  jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
+    console.log(err);
+
+    if (err) return res.sendStatus(403);
+
+    req.user = user;
+
+    next();
+  });
+}
+
 app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
@@ -22,7 +43,9 @@ app.put("/users/:id", db.updateUser);
 app.delete("/userDelete", db.deleteUser);
 app.post("/otp", db.checkOtp);
 app.post("/resendOtp", db.resendOtp);
-app.get("/restaurant", db.getRestaurantWithMenu);
+app.get("/restaurantwithmenu", db.getRestaurantWithMenu);
+app.get("/restaurant", authenticateToken, db.getRestaurant);
+app.post("/restaurant", db.createRestaurantAdmin);
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}.`);
